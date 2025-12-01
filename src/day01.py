8dc76612f806b33a7310f9e8e0dac25ae716df8c
Ken Chan <kenchan12348@gmail.com>
2025-12-01 00:52:25 -0500

    Day1

--- src/day01.py
+++ src/day01.py
@@ -1,5 +1,31 @@
+from utils import *
+
+instructions = read_input(1)
+
 def part1():
-    pass
+    dial = 50
+    count = 0
+    for inst in instructions:
+        dir = inst[0]
+        val = int(inst[1:])
+        dial = (dial + (1 if dir == 'R' else -1) * (val)) % 100
+        if dial == 0:
+            count += 1
+    print(count)
+
 
 def part2():
-    pass
+    instructions = read_input(1)
+    dial = 50
+    count = 0
+    for inst in instructions:
+        dir = inst[0]
+        val = int(inst[1:])
+        for i in range(val):
+            dial = (dial + (1 if dir == 'R' else -1)) % 100
+            if dial == 0:
+                count += 1
+    print(count)
+
+part1()
+part2()
